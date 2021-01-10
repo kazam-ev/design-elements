ba64a467813e16d26137f3fe4691efaec7bbcf80
sarangvasishth <sarang.vasishth.@gmail.com>
2021-01-10 16:04:50 +0530

    new build

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4bbe76063006045259bc";
+/******/ 	var hotCurrentHash = "adb49aec684cd157c9eb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
