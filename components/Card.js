5cbfccb4e9041c44d844dc35197c125059083f58
sarangvasishth <sarang.vasishth.@gmail.com>
2021-01-10 16:07:19 +0530

    build

--- components/Card.js
+++ components/Card.js
@@ -1,29 +1,23 @@
 const Card = (props) => {
-    return (
-        <div>
-            
-
-            <div >
-                <section id="facilities ">
-                    <div className="">
-                            
-                                <div className="cd">
-                                    <div className="card-text-center">
-                                        <img src={props.imgUrl} class="card-img-top" />
-                                        <div className="card-body">
-                                            <h5 className="card-title">{props.cardTitle}</h5>
-                                            <p className="card-text">
-                                                {props.cardText}
-                                            </p>
-                                        </div>
-                                    </div>
-                            </div>
-                    </div>
-                </section>
+  return (
+    <div>
+      <div>
+        <section id="facilities ">
+          <div className="">
+            <div className="cd">
+              <div className="card-text-center">
+                <img src={props.imgUrl} className="card-img-top" />
+                <div className="card-body">
+                  <h5 className="card-title">{props.cardTitle}</h5>
+                  <p className="card-text">{props.cardText}</p>
+                </div>
+              </div>
             </div>
-           
-        </div>
-    );
-}
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
